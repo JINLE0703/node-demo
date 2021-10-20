bf8663341a297eded8b3972491c23f60c53a3925
jinle <jinle0703@foxmail.com>
2021-10-20 23:33:54 +0800

    feat: http-game

---
+++ http-game/index.js
@@ -0,0 +1,50 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const querystring = require('querystring')
+
+const game = require('./game')
+
+let playerWon = 0
+
+http.createServer((request, response) => {
+    const parseUrl = url.parse(request.url)
+
+    if (request.url === '/favicon.ico') {
+        response.writeHead(200)
+        response.end()
+        return
+    }
+
+    if(parseUrl.pathname === '/') {
+        response.writeHead(200)
+        fs.createReadStream(__dirname + '/index.html').pipe(response)
+    }
+
+    if(parseUrl.pathname === '/game') {
+        const query = querystring.parse(parseUrl.query)
+        const playAction = query.action
+
+        const gameResult = game(playAction)
+        
+        if (playerWon >= 3) {
+            response.writeHead(500)
+            response.end('我不和你玩了！')
+            return
+        }
+
+        response.writeHead(200)
+        if (gameResult === 0) {
+            response.end('平局啦！')
+        } else if (gameResult === 1) {
+            playerWon++
+            response.end('你赢啦！')
+        } else {
+            response.end('你输啦！')
+        }
+        
+    }
+    
+}).listen(3000)
+
+console.log('server is running')
